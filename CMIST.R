99136ae10b98c33c8f1200bf1803a6bd6acbbd79
Norah Brown <norahbrown@uvic.ca>
2021-03-22 16:51:58 -0700

    tried it out for redswamp crayfish. There's something wrong with calculation since rs should be more certain (i.e. small CIs) than marble

--- CMIST.R
+++ CMIST.R
@@ -40,7 +40,7 @@ ggplot(score, aes(y=CMIST_Score, x=rownames(score))) +
   geom_point()
 
 
-marble_risk<-read.csv("C:Inputs//marble_score.csv")
+marble_risk<-read.csv("C:Inputs//marble_risk.csv")
 marble_risk
 rownames(marble_risk)<-marble_risk$region
 marble_risk_simple<-marble_risk[,-c(1:3)]
@@ -65,8 +65,8 @@ score_marble <- data.frame(matrix(ncol = 9, nrow = 0))
 
 set.seed(11)
 for (r in 1:21) {
-    new<-CMISTScore(as.numeric(marble_risk_simple_1_3[r,]),as.numeric(marble_uncertainties_simple_1_3[r,]))
-    score_marble[nrow(score_marble)+1, ]<-new
+    new_marble<-CMISTScore(as.numeric(marble_risk_simple_1_3[r,]),as.numeric(marble_uncertainties_simple_1_3[r,]))
+    score_marble[nrow(score_marble)+1, ]<-new_marble
     }
 
     
@@ -79,3 +79,72 @@ colnames(score_marble)<-colnames(score)
 ggplot(score_marble, aes(y=CMIST_Score, x=rownames(score_marble))) + 
   geom_errorbar(aes(ymin=CMIST_Lower, ymax=CMIST_Upper), width=.1) +
   geom_point()
+
+
+############### redswamp
+redswamp_risk<-read.csv("C:Inputs//redswamp_risk.csv")
+redswamp_risk
+rownames(redswamp_risk)<-redswamp_risk$region
+redswamp_risk_simple<-redswamp_risk[,-c(1:3)]
+redswamp_risk_simple
+redswamp_risk_simple_1_3<-redswamp_risk_simple+1
+redswamp_risk_simple_1_3
+colnames(redswamp_risk_simple_1_3)<-NULL
+rownames(redswamp_risk_simple_1_3)<-NULL
+redswamp_risk_simple_1_3
+dim(redswamp_risk_simple_1_3)
+
+redswamp_uncertainties<-read.csv("C:Inputs//redswamp_uncertainty.csv")
+rownames(redswamp_uncertainties)<-redswamp_uncertainties$ID
+redswamp_uncertainties_simple<-redswamp_uncertainties[,-c(1:3)]
+redswamp_uncertainties_simple_1_3<-redswamp_uncertainties_simple+1
+colnames(redswamp_uncertainties_simple_1_3)<-NULL
+rownames(redswamp_uncertainties_simple_1_3)<-NULL
+redswamp_uncertainties_simple_1_3
+
+
+score_redswamp <- data.frame(matrix(ncol = 9, nrow = 0))
+
+set.seed(11)
+for (r in 1:21) {
+  new_redswamp<-CMISTScore(as.numeric(redswamp_risk_simple_1_3[r,]),as.numeric(redswamp_uncertainties_simple_1_3[r,]))
+  score_redswamp[nrow(score_redswamp)+1, ]<-new_redswamp
+}
+
+
+score_redswamp    
+rownames(score_redswamp)<-rownames(redswamp_risk_simple)
+colnames(score_redswamp)<-colnames(score)
+
+
+
+ggplot(score_redswamp, aes(y=CMIST_Score, x=rownames(score_redswamp))) + 
+  geom_errorbar(aes(ymin=CMIST_Lower, ymax=CMIST_Upper), width=.1) +
+  geom_point()
+
+score_redswamp_id<-score_redswamp
+score_redswamp_id$region<-rownames(score_redswamp_id)
+score_redswamp_id$species<-"redswamp"
+score_redswamp_id
+
+score_marble_id<-score_marble
+score_marble_id$region<-rownames(score_marble_id)
+score_marble_id$species<-"marble"
+score_marble_id
+
+scores_combined<-rbind(score_marble_id, score_redswamp_id)
+head(scores_combined)
+
+ggplot(scores_combined, aes(y=CMIST_Score, x=region, colour=species)) + 
+  geom_errorbar(aes(ymin=CMIST_Lower, ymax=CMIST_Upper), width=.1, position=position_dodge(width=0.5)) +
+  geom_point(size=4, position=position_dodge(width=0.5))
+
+
+#Likelihood
+ggplot(scores_combined, aes(y=Likelihood_Score, x=region, colour=species)) + 
+  geom_errorbar(aes(ymin=Likelihood_Lower, ymax=Likelihood_Upper), width=.1, position=position_dodge(width=0.5)) +
+  geom_point(size=4, position=position_dodge(width=0.5))
+#Impact
+ggplot(scores_combined, aes(y=Impact_Score, x=region, colour=species)) + 
+  geom_errorbar(aes(ymin=Impact_Lower, ymax=Impact_Upper), width=.1, position=position_dodge(width=0.5)) +
+  geom_point(size=4, position=position_dodge(width=0.5))
