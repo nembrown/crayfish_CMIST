ec5f9e5c29b897a3891f98a0d3f057a1ebad98d4
Norah Brown <norahbrown@uvic.ca>
2021-03-23 11:10:58 -0700

    added the data in for the rest of the crayfish, plotted comparison across all regions

--- CMIST.R
+++ CMIST.R
@@ -40,6 +40,8 @@ ggplot(score, aes(y=CMIST_Score, x=rownames(score))) +
   geom_point()
 
 
+### marble
+
 marble_risk<-read.csv("C:Inputs//marble_risk.csv")
 marble_risk
 rownames(marble_risk)<-marble_risk$region
@@ -74,7 +76,10 @@ score_marble
 rownames(score_marble)<-rownames(marble_risk_simple)
 colnames(score_marble)<-colnames(score)
 
-
+score_marble_id<-score_marble
+score_marble_id$region<-rownames(score_marble_id)
+score_marble_id$species<-"marble"
+score_marble_id
 
 ggplot(score_marble, aes(y=CMIST_Score, x=rownames(score_marble))) + 
   geom_errorbar(aes(ymin=CMIST_Lower, ymax=CMIST_Upper), width=.1) +
@@ -116,23 +121,286 @@ score_redswamp
 rownames(score_redswamp)<-rownames(redswamp_risk_simple)
 colnames(score_redswamp)<-colnames(score)
 
-
-
-ggplot(score_redswamp, aes(y=CMIST_Score, x=rownames(score_redswamp))) + 
-  geom_errorbar(aes(ymin=CMIST_Lower, ymax=CMIST_Upper), width=.1) +
-  geom_point()
-
 score_redswamp_id<-score_redswamp
 score_redswamp_id$region<-rownames(score_redswamp_id)
 score_redswamp_id$species<-"redswamp"
 score_redswamp_id
 
-score_marble_id<-score_marble
-score_marble_id$region<-rownames(score_marble_id)
-score_marble_id$species<-"marble"
-score_marble_id
+ggplot(score_redswamp, aes(y=CMIST_Score, x=rownames(score_redswamp))) + 
+  geom_errorbar(aes(ymin=CMIST_Lower, ymax=CMIST_Upper), width=.1) +
+  geom_point()
 
-scores_combined<-rbind(score_marble_id, score_redswamp_id)
+######## whiteriver
+whiteriver_risk<-read.csv("C:Inputs//whiteriver_risk.csv")
+whiteriver_risk
+rownames(whiteriver_risk)<-whiteriver_risk$region
+whiteriver_risk_simple<-whiteriver_risk[,-c(1:3)]
+whiteriver_risk_simple
+whiteriver_risk_simple_1_3<-whiteriver_risk_simple+1
+whiteriver_risk_simple_1_3
+colnames(whiteriver_risk_simple_1_3)<-NULL
+rownames(whiteriver_risk_simple_1_3)<-NULL
+whiteriver_risk_simple_1_3
+dim(whiteriver_risk_simple_1_3)
+
+whiteriver_uncertainties<-read.csv("C:Inputs//whiteriver_certainty_321.csv")
+rownames(whiteriver_uncertainties)<-whiteriver_uncertainties$ID
+whiteriver_uncertainties_simple<-whiteriver_uncertainties[,-c(1:3)]
+whiteriver_uncertainties_simple_1_3<-whiteriver_uncertainties_simple
+colnames(whiteriver_uncertainties_simple_1_3)<-NULL
+rownames(whiteriver_uncertainties_simple_1_3)<-NULL
+whiteriver_uncertainties_simple_1_3
+
+
+score_whiteriver <- data.frame(matrix(ncol = 9, nrow = 0))
+
+set.seed(11)
+for (r in 1:21) {
+  new_whiteriver<-CMISTScore(as.numeric(whiteriver_risk_simple_1_3[r,]),as.numeric(whiteriver_uncertainties_simple_1_3[r,]))
+  score_whiteriver[nrow(score_whiteriver)+1, ]<-new_whiteriver
+}
+
+
+score_whiteriver    
+rownames(score_whiteriver)<-rownames(whiteriver_risk_simple)
+colnames(score_whiteriver)<-colnames(score)
+
+score_whiteriver_id<-score_whiteriver
+score_whiteriver_id$region<-rownames(score_whiteriver_id)
+score_whiteriver_id$species<-"whiteriver"
+score_whiteriver_id
+
+ggplot(score_whiteriver, aes(y=CMIST_Score, x=rownames(score_whiteriver))) + 
+  geom_errorbar(aes(ymin=CMIST_Lower, ymax=CMIST_Upper), width=.1) +
+  geom_point()
+
+
+############### rusty
+rusty_risk<-read.csv("C:Inputs//rusty_risk.csv")
+rusty_risk
+rownames(rusty_risk)<-rusty_risk$region
+rusty_risk_simple<-rusty_risk[,-c(1:3)]
+rusty_risk_simple
+rusty_risk_simple_1_3<-rusty_risk_simple+1
+rusty_risk_simple_1_3
+colnames(rusty_risk_simple_1_3)<-NULL
+rownames(rusty_risk_simple_1_3)<-NULL
+rusty_risk_simple_1_3
+dim(rusty_risk_simple_1_3)
+
+rusty_uncertainties<-read.csv("C:Inputs//rusty_certainty_321.csv")
+rownames(rusty_uncertainties)<-rusty_uncertainties$ID
+rusty_uncertainties_simple<-rusty_uncertainties[,-c(1:3)]
+rusty_uncertainties_simple_1_3<-rusty_uncertainties_simple
+colnames(rusty_uncertainties_simple_1_3)<-NULL
+rownames(rusty_uncertainties_simple_1_3)<-NULL
+rusty_uncertainties_simple_1_3
+
+
+score_rusty <- data.frame(matrix(ncol = 9, nrow = 0))
+
+set.seed(11)
+for (r in 1:21) {
+  new_rusty<-CMISTScore(as.numeric(rusty_risk_simple_1_3[r,]),as.numeric(rusty_uncertainties_simple_1_3[r,]))
+  score_rusty[nrow(score_rusty)+1, ]<-new_rusty
+}
+
+
+score_rusty    
+rownames(score_rusty)<-rownames(rusty_risk_simple)
+colnames(score_rusty)<-colnames(score)
+
+score_rusty_id<-score_rusty
+score_rusty_id$region<-rownames(score_rusty_id)
+score_rusty_id$species<-"rusty"
+score_rusty_id
+
+ggplot(score_rusty, aes(y=CMIST_Score, x=rownames(score_rusty))) + 
+  geom_errorbar(aes(ymin=CMIST_Lower, ymax=CMIST_Upper), width=.1) +
+  geom_point()
+
+
+
+############### spinycheek
+spinycheek_risk<-read.csv("C:Inputs//spinycheek_risk.csv")
+spinycheek_risk
+rownames(spinycheek_risk)<-spinycheek_risk$region
+spinycheek_risk_simple<-spinycheek_risk[,-c(1:3)]
+spinycheek_risk_simple
+spinycheek_risk_simple_1_3<-spinycheek_risk_simple+1
+spinycheek_risk_simple_1_3
+colnames(spinycheek_risk_simple_1_3)<-NULL
+rownames(spinycheek_risk_simple_1_3)<-NULL
+spinycheek_risk_simple_1_3
+dim(spinycheek_risk_simple_1_3)
+
+spinycheek_uncertainties<-read.csv("C:Inputs//spinycheek_certainty_321.csv")
+rownames(spinycheek_uncertainties)<-spinycheek_uncertainties$ID
+spinycheek_uncertainties_simple<-spinycheek_uncertainties[,-c(1:3)]
+spinycheek_uncertainties_simple_1_3<-spinycheek_uncertainties_simple
+colnames(spinycheek_uncertainties_simple_1_3)<-NULL
+rownames(spinycheek_uncertainties_simple_1_3)<-NULL
+spinycheek_uncertainties_simple_1_3
+
+
+score_spinycheek <- data.frame(matrix(ncol = 9, nrow = 0))
+
+set.seed(11)
+for (r in 1:21) {
+  new_spinycheek<-CMISTScore(as.numeric(spinycheek_risk_simple_1_3[r,]),as.numeric(spinycheek_uncertainties_simple_1_3[r,]))
+  score_spinycheek[nrow(score_spinycheek)+1, ]<-new_spinycheek
+}
+
+
+score_spinycheek    
+rownames(score_spinycheek)<-rownames(spinycheek_risk_simple)
+colnames(score_spinycheek)<-colnames(score)
+
+score_spinycheek_id<-score_spinycheek
+score_spinycheek_id$region<-rownames(score_spinycheek_id)
+score_spinycheek_id$species<-"spinycheek"
+score_spinycheek_id
+
+ggplot(score_spinycheek, aes(y=CMIST_Score, x=rownames(score_spinycheek))) + 
+  geom_errorbar(aes(ymin=CMIST_Lower, ymax=CMIST_Upper), width=.1) +
+  geom_point()
+
+############### virile
+virile_risk<-read.csv("C:Inputs//virile_risk.csv")
+virile_risk
+rownames(virile_risk)<-virile_risk$region
+virile_risk_simple<-virile_risk[,-c(1:3)]
+virile_risk_simple
+virile_risk_simple_1_3<-virile_risk_simple+1
+virile_risk_simple_1_3
+colnames(virile_risk_simple_1_3)<-NULL
+rownames(virile_risk_simple_1_3)<-NULL
+virile_risk_simple_1_3
+dim(virile_risk_simple_1_3)
+
+virile_uncertainties<-read.csv("C:Inputs//virile_certainty_321.csv")
+rownames(virile_uncertainties)<-virile_uncertainties$ID
+virile_uncertainties_simple<-virile_uncertainties[,-c(1:3)]
+virile_uncertainties_simple_1_3<-virile_uncertainties_simple
+colnames(virile_uncertainties_simple_1_3)<-NULL
+rownames(virile_uncertainties_simple_1_3)<-NULL
+virile_uncertainties_simple_1_3
+
+
+score_virile <- data.frame(matrix(ncol = 9, nrow = 0))
+
+set.seed(11)
+for (r in 1:16) {
+  new_virile<-CMISTScore(as.numeric(virile_risk_simple_1_3[r,]),as.numeric(virile_uncertainties_simple_1_3[r,]))
+  score_virile[nrow(score_virile)+1, ]<-new_virile
+}
+
+
+score_virile    
+rownames(score_virile)<-rownames(virile_risk_simple)
+colnames(score_virile)<-colnames(score)
+
+score_virile_id<-score_virile
+score_virile_id$region<-rownames(score_virile_id)
+score_virile_id$species<-"virile"
+score_virile_id
+
+ggplot(score_virile, aes(y=CMIST_Score, x=rownames(score_virile))) + 
+  geom_errorbar(aes(ymin=CMIST_Lower, ymax=CMIST_Upper), width=.1) +
+  geom_point()
+
+############### signal
+signal_risk<-read.csv("C:Inputs//signal_risk.csv")
+signal_risk
+rownames(signal_risk)<-signal_risk$region
+signal_risk_simple<-signal_risk[,-c(1:3)]
+signal_risk_simple
+signal_risk_simple_1_3<-signal_risk_simple+1
+signal_risk_simple_1_3
+colnames(signal_risk_simple_1_3)<-NULL
+rownames(signal_risk_simple_1_3)<-NULL
+signal_risk_simple_1_3
+dim(signal_risk_simple_1_3)
+
+signal_uncertainties<-read.csv("C:Inputs//signal_certainty_321.csv")
+rownames(signal_uncertainties)<-signal_uncertainties$ID
+signal_uncertainties_simple<-signal_uncertainties[,-c(1:3)]
+signal_uncertainties_simple_1_3<-signal_uncertainties_simple
+colnames(signal_uncertainties_simple_1_3)<-NULL
+rownames(signal_uncertainties_simple_1_3)<-NULL
+signal_uncertainties_simple_1_3
+
+
+score_signal <- data.frame(matrix(ncol = 9, nrow = 0))
+
+set.seed(11)
+for (r in 1:20) {
+  new_signal<-CMISTScore(as.numeric(signal_risk_simple_1_3[r,]),as.numeric(signal_uncertainties_simple_1_3[r,]))
+  score_signal[nrow(score_signal)+1, ]<-new_signal
+}
+
+
+score_signal    
+rownames(score_signal)<-rownames(signal_risk_simple)
+colnames(score_signal)<-colnames(score)
+
+score_signal_id<-score_signal
+score_signal_id$region<-rownames(score_signal_id)
+score_signal_id$species<-"signal"
+score_signal_id
+
+ggplot(score_signal, aes(y=CMIST_Score, x=rownames(score_signal))) + 
+  geom_errorbar(aes(ymin=CMIST_Lower, ymax=CMIST_Upper), width=.1) +
+  geom_point()
+
+############### allegheny
+allegheny_risk<-read.csv("C:Inputs//allegheny_risk.csv")
+allegheny_risk
+rownames(allegheny_risk)<-allegheny_risk$region
+allegheny_risk_simple<-allegheny_risk[,-c(1:3)]
+allegheny_risk_simple
+allegheny_risk_simple_1_3<-allegheny_risk_simple+1
+allegheny_risk_simple_1_3
+colnames(allegheny_risk_simple_1_3)<-NULL
+rownames(allegheny_risk_simple_1_3)<-NULL
+allegheny_risk_simple_1_3
+dim(allegheny_risk_simple_1_3)
+
+allegheny_uncertainties<-read.csv("C:Inputs//allegheny_certainty_321.csv")
+rownames(allegheny_uncertainties)<-allegheny_uncertainties$ID
+allegheny_uncertainties_simple<-allegheny_uncertainties[,-c(1:3)]
+allegheny_uncertainties_simple_1_3<-allegheny_uncertainties_simple
+colnames(allegheny_uncertainties_simple_1_3)<-NULL
+rownames(allegheny_uncertainties_simple_1_3)<-NULL
+allegheny_uncertainties_simple_1_3
+
+
+score_allegheny <- data.frame(matrix(ncol = 9, nrow = 0))
+
+set.seed(11)
+for (r in 1:21) {
+  new_allegheny<-CMISTScore(as.numeric(allegheny_risk_simple_1_3[r,]),as.numeric(allegheny_uncertainties_simple_1_3[r,]))
+  score_allegheny[nrow(score_allegheny)+1, ]<-new_allegheny
+}
+
+
+score_allegheny    
+rownames(score_allegheny)<-rownames(allegheny_risk_simple)
+colnames(score_allegheny)<-colnames(score)
+
+score_allegheny_id<-score_allegheny
+score_allegheny_id$region<-rownames(score_allegheny_id)
+score_allegheny_id$species<-"allegheny"
+score_allegheny_id
+
+ggplot(score_allegheny, aes(y=CMIST_Score, x=rownames(score_allegheny))) + 
+  geom_errorbar(aes(ymin=CMIST_Lower, ymax=CMIST_Upper), width=.1) +
+  geom_point()
+
+
+#### combining
+
+scores_combined<-rbind(score_marble_id, score_redswamp_id, score_whiteriver_id, score_rusty_id, score_spinycheek_id, score_virile_id, score_signal_id, score_allegheny_id)
 head(scores_combined)
 
 ggplot(scores_combined, aes(y=CMIST_Score, x=region, colour=species)) + 
